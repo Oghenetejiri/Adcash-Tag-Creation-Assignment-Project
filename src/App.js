ff1b9a85a9bc5240f149023c30a0568547ad97f4
Oghenetejiri <78227200+Oghenetejiri@users.noreply.github.com>
2021-03-22 19:55:05 +0200

    creation of the tags

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import { useState } from "react";
 import './App.css';
 
 function App() {
+  let [tags, setTags] = useState([]);
+  let addTag = (e) => {
+    
+    if (e.key === "Enter") {
+      if (e.target.value.length > 0) {
+        setTags([...tags, e.target.value]);
+        e.target.value = "";
+        
+      }
+    }
+  };
+  let removeTag = (removeTag) => {
+    let newTags = tags.filter((tag) => tag !== removeTag);
+    setTags(newTags);
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src="https://adcash.com/wp-content/themes/adcash/assets/dist/img/logo-adcash.svg"></img>
+         <h1>Adcash tag creation assignment-FrontEnd Intern </h1>
+      <h3>Please enter the number of times you have travelled out of Estonia in the following years respectively:</h3>
+      <ul>
+      <li>2019</li>
+      <li>2020</li>
+      <li>2021</li>
+      </ul>
+      <div className="tag-container">
+        {tags.map((tag, index) => {
+          return (
+            <div key={index} className="tag">
+              {tag}
+              <span className="nice" onClick={() => removeTag(tag)}>x</span>
+            </div>
+          );
+        })}
+
+        <input type ="number" placeholder="Input digit and press enter" onKeyDown={addTag} />
+      </div>
       </header>
+      
     </div>
   );
 }
+   
 
 export default App;
